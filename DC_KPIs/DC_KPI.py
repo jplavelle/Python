bb8f837218e3a9c862e831bc025acce26a166020
Jacob <jacob.p.lavelle@gmail.com>
2016-09-04 17:05:01 -0400

    Code updates before testing

--- DC_KPIs/DC_KPI.py
+++ DC_KPIs/DC_KPI.py
@@ -1,23 +1,63 @@
+# This program compiles KPIs (key performance indicators), reported by the
+# government of the District of Columbia, for the selected year and budget
+# program. The result is saved to a CSV file for additional processing.
+
 import csv
 import sys
 
-print ('Ensure the relevant files are present in the same directory.')
-reporting_year = raw_input('Enter the reporting year you wish to append to the output file.>>>')
+def data_in(year):
+    csv.reader(open('DC_Agency_Performance_Data_KPIs__%s.csv', 'rb')) % year
 
-input_file = 'DC_Agency_Performance_Data_KPIs__%s.csv' % reporting_year
+def set_KPI_data(year):
+    csv.reader(open('DC_Agency_Performance_Data_KPIs__%s.csv', 'rb')) % year
 
-KPI = csv.reader(open(input_file, 'rb'))
+# Confirm source files are properly staged.
+prep_check = raw_input("""Please ensure source files are in the same directory as this script.
+Are you ready to proceed?
+'Y' or 'N'""")
 
-header = KPI.next()
+# If source files are not staged, the script will close.
+# If ready, then the user is prompted to enter list of reporting years.
+if prep_check = 'N':
+    print ("Please have all relevant files prepared before proceeding.")
+    exit
+elif
+    year_input = raw_input('Enter reporting years to compile, separated by a space.>>>')
+    year_list = year_input.split(' ')
 
-hsp = [(header)]
+# Enter the budget program for which to compile KPIs.
+program_input = raw_input('Enter the budget program you wish to compile.>>>')
+budget_program = program_input.upper()
 
-for row in KPI:
-    if row[17] == 'HOMELESS SERVICES PROGRAM':
-        hsp.append(row)
+# Set first reporting year.
+initial_year = year_list[0]
 
-print hsp
+# Save out headers and initialize target list.
+KPI_data = set_KPI_data(initial_year)
+header = KPI_data.next()
+program_compiled = [(header)]
 
-with open('Human_Services_KPIs.csv','wb') as output_path:
+# Loop through reporting years and budget programs.
+i = 0
+
+While i <=len(year_list) + 1:
+    reporting_year = year_list[i]
+    KPI_data = set_KPI_data(reporting_year)
+
+    for row in KPI_data:
+        if row[17] == budget_program:
+            program_compiled.append(row)
+    i =+ 1
+
+
+# Update of results and saving results to CSV.
+print 'Data to be saved out is as follows:\n%s' % program_compiled
+
+
+with open('%s_KPIs.csv','wb') % budget_program as output_path:
     output_file = csv.writer(output_path)
-    output_file.writerows(hsp)
+    output_file.writerows(program_compiled)
+
+print ("""
+Success! All your base code are belong to us...
+Just kidding.")
