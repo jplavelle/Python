2ab04a10e95731274a3613595b8c0e80fe1f79fd
Jacob <jacob.p.lavelle@gmail.com>
2016-09-04 21:47:02 -0400

    Updates following first round of testing.

--- DC_KPIs/DC_KPI.py
+++ DC_KPIs/DC_KPI.py
@@ -5,25 +5,28 @@
 import csv
 import sys
 
-def data_in(year):
-    csv.reader(open('DC_Agency_Performance_Data_KPIs__%s.csv', 'rb')) % year
+#def source_file(year):
+#    return """open('DC_Agency_Performance_Data_KPIs__%s.csv', 'rb')""" % year
 
 def set_KPI_data(year):
-    csv.reader(open('DC_Agency_Performance_Data_KPIs__%s.csv', 'rb')) % year
+    filestring = 'DC_Agency_Performance_Data_KPIs__%s.csv' % year
+    source_file = open(filestring, 'rb')
+    source_data = csv.reader(source_file)
+    return source_data
 
 # Confirm source files are properly staged.
 prep_check = raw_input("""Please ensure source files are in the same directory as this script.
 Are you ready to proceed?
-'Y' or 'N'""")
+'Y' or 'N'>>>""")
 
 # If source files are not staged, the script will close.
 # If ready, then the user is prompted to enter list of reporting years.
-if prep_check = 'N':
-    print ("Please have all relevant files prepared before proceeding.")
-    exit
-elif
+if prep_check == 'Y':
     year_input = raw_input('Enter reporting years to compile, separated by a space.>>>')
     year_list = year_input.split(' ')
+else:
+    print ("Please have all relevant files prepared before proceeding.")
+    exit(0)
 
 # Enter the budget program for which to compile KPIs.
 program_input = raw_input('Enter the budget program you wish to compile.>>>')
@@ -39,9 +42,11 @@ program_compiled = [(header)]
 
 # Loop through reporting years and budget programs.
 i = 0
+range_end = len(year_list)
 
-While i <=len(year_list) + 1:
+for i in range (0, range_end):
     reporting_year = year_list[i]
+    print "Processing %s" % reporting_year
     KPI_data = set_KPI_data(reporting_year)
 
     for row in KPI_data:
@@ -50,14 +55,14 @@ While i <=len(year_list) + 1:
     i =+ 1
 
 
-# Update of results and saving results to CSV.
-print 'Data to be saved out is as follows:\n%s' % program_compiled
-
-
-with open('%s_KPIs.csv','wb') % budget_program as output_path:
-    output_file = csv.writer(output_path)
-    output_file.writerows(program_compiled)
+# Results saved to CSV.
+output_string = 'DC_Performance_KPIs_%s.csv' % budget_program
+output_path = open(output_string,'wb')
+output_file = csv.writer(output_path)
+output_file.writerows(program_compiled)
+print """Results are saved to DC_Performance_KPIs_%s.csv""" % budget_program
 
 print ("""
 Success! All your base code are belong to us...
-Just kidding.")
+Just kidding.
+I've been wanting to use that line for a while.""")
